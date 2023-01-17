275c43bd64de391a9f1ff76235d720a657bf3d48
Will <wdk1g19@soton.ac.uk>
2023-01-17 16:10:48 +0000

    Add types file

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,13 @@
 import babel from "rollup-plugin-babel";
 import commonjs from "rollup-plugin-commonjs";
 import external from "rollup-plugin-peer-deps-external";
-
+import typescript from "rollup-plugin-typescript";
 import resolve from "rollup-plugin-node-resolve";
 
 import svgr from "@svgr/rollup";
 
 export default {
-  input: "index.js",
+  input: "index.ts",
   output: {
     file: "dist/bundle.js",
     format: "es",
@@ -17,9 +17,14 @@ export default {
     svgr(),
     babel({
       exclude: "node_modules/**",
+      extensions: [".js", ".ts", ".tsx", ".jsx"],
       // plugins: [ '@babel/external-helpers' ]
     }),
     resolve(),
     commonjs(),
+    typescript({
+      declaration: true /* Generates corresponding '.d.ts' file. */,
+      declarationDir: `dist/types`,
+    }),
   ],
 };
